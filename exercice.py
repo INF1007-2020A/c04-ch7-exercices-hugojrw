83c19403c28c1842552d7bf5c5e34da43df52911
hugojrw <70610964+hugojrw@users.noreply.github.com>
2020-10-19 18:41:11 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -4,11 +4,13 @@
 # TODO: Importez vos modules ici
 
 
-
 # TODO: Définissez vos fonction ici
-
+def compute_volume_mass(a ,b ,c ,p):
+    volume=(4/3)*math.pi**a*b*c
+    mass=p*volume
+    return mass,volume
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
-
+    print(compute_volume_mass(2, 4, 2, 10))
     pass
