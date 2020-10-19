a469af73a03d27abb2811f56fb278d8b01fc938a
hugojrw <70610964+hugojrw@users.noreply.github.com>
2020-10-19 19:32:57 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # TODO: Importez vos modules ici
-
+from turtle import *
 
 # TODO: Définissez vos fonction ici
 def compute_volume_mass(a ,b ,c ,p):
@@ -10,7 +10,25 @@ def compute_volume_mass(a ,b ,c ,p):
     mass=p*volume
     return mass,volume
 
+def tree():
+  setheading(90)
+  color('green')
+  speed(0)
+  branch()
+
+def branch(length=70, pen=7, angle=35):
+  if length > 0 and pen > 0:  
+    pensize(pen)
+    forward(length)
+    right(angle)
+    branch(length-10, pen-1, angle-5)
+    left(angle*2)
+    branch(length-10, pen-1, angle-5)
+    right(angle)
+    backward(length)
+
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
     print(compute_volume_mass(2, 4, 2, 10))
+    tree()
     pass
